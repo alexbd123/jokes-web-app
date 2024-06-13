f7737f665815a9dbb0af1f7f7b94aa5ed6a0ac65
alexbd123 <alexpjbd@gmail.com>
2024-06-13 12:09:18 +0100

    add joke props

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,7 +1,10 @@
 import React from "react"
 
-export default function Joke() {
+export default function Joke(props) {
     return (
-        <h1>This is a joke</h1>
+        <div>
+            <h1>{props.setup}</h1>
+            <h2>{props.punchline}</h2>
+        </div>
     )
 }
